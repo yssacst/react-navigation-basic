cc6000e9a09140fb88734f76bbcc3673fd69c1d4
yssacst <rayssacoosta@gmail.com>
2019-09-07 23:54:09 -0300

    tabnativebase

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native'
-import { TabNavigator } from 'react-navigation'
+import { View ,StyleSheet} from 'react-native';
+  
+  import {Container, Tab, Tabs,Text } from 'native-base';
 
 import MinhaEstanteScreen from './MinhaEstanteScreen.js'
 import LojaScreen from './LojaScreen.js'
 
-var MainScreenNavigator =  TabNavigator({
-    MinhaEstante:{ screen: MinhaEstanteScreen},
-    Loja:{ screen: LojaScreen},
+export default class HomeScreen extends Component {
+  
+    render() {
+      return (
+        <Container style={styles.container}>
+            <Tabs initialPage={0} tabBarUnderlineStyle={{backgroundColor:"#83b582"}}>
+              <Tab heading="Minha Estante" activeTextStyle={{color: "#83b582"}} tabStyle={{ backgroundColor: "#f0dd92" }} activeTabStyle={{ backgroundColor: "#ffffc5" }}>
+                <MinhaEstanteScreen/>
+              </Tab>
+              <Tab heading="Loja" activeTextStyle={{color: "#83b582"}}  tabStyle={{ backgroundColor: "#f0dd92" }} activeTabStyle={{ backgroundColor: "#ffffc5" }}>
+                 <LojaScreen/>
+              </Tab>
+            </Tabs>
+          </Container>
+      );
+    }
+  }
 
-});
-
-MainScreenNavigator.navigationOptions = {
-    title: 'Tab Exemples'
-}
-
-export default MainScreenNavigator;
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      marginTop:25
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
--- src/screens/LojaScreen.js
+++ src/screens/LojaScreen.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { Text, StyleSheet, Dimensions, View, ImageBackground } from 'react-native'
 import { TouchableHighlight, TextInput } from 'react-native-gesture-handler';
 
-export default class LojaScreen extends Component {
-  
+import bgImage from '../images/img3.jpg';
+const { width : WIDTH} = Dimensions.get('window');
+
+export default class LojaScreen extends Component {  
     render(){
         return(
+            <ImageBackground source={bgImage} imageStyle={{opacity:0.5}} style={styles.backgroundContainer}>
                 <View style={styles.container}>
                     <Text>Loja</Text>
                 </View>
+            </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    backgroundContainer:{
+        flex:1,
+        width:null,
+        height:null,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: '#f0dd92'
+    },
    container:{
        flex:1,
        justifyContent:'center',
